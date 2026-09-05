ba24416946c8c71a818919b2e86b8e1fe9237f53
coding67-KR <a51286652@gmail.com>
2026-09-05 15:08:27 +0900

    fix: track game result in state

--- src/main/kotlin/GameState.kt
+++ src/main/kotlin/GameState.kt
@@ -5,6 +5,9 @@ class GameState {
     var isFinished: Boolean = false
         private set
 
+    var result: GameResult? = null
+        private set
+
     val questionNumber: Int
         get() = history.size
 
@@ -14,10 +17,13 @@ class GameState {
 
     fun record(question: Question, answer: Answer) {
         history += QuestionAnswer(question, answer)
-        if (history.size >= GameRules.MAX_QUESTIONS) isFinished = true
+        if (history.size >= GameRules.MAX_QUESTIONS && !isFinished) {
+            finish(GameResult.OUT_OF_QUESTIONS)
+        }
     }
 
-    fun finish() {
+    fun finish(gameResult: GameResult) {
         isFinished = true
+        result = gameResult
     }
 }
